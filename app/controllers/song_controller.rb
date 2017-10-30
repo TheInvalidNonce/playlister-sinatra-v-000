3c8a9d99fe6eb04ca038066d98250c001c1b47e5
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-30 23:10:50 +0000

    automatically backed up by learn

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -1,16 +1,65 @@
+# require 'rack-flash'
+# class SongController < ApplicationController
+# 
+#   # This should present the user with a list of all songs in the library.
+#   # Each song should be a clickable link to that particular song's show page.
+#   get '/songs' do
+#     @songs = Song.all
+#     erb :'songs/index'
+#   end
+# 
+#   # Display the new song form with selectable genres
+#   get '/songs/new' do
+#     @genres = Genre.all
+#     erb :'/songs/new'
+#   end
+# 
+#   post '/songs' do
+#     artist = Artist.find_or_create_by(name: params[:song][:artist_name])
+#     song = Song.create(name: params[:song][:name], artist: artist)
+#     song.genre_ids = params[:song][:genres]
+#     song.save
+# 
+#     flash[:message] = "Sucessfully created song."
+#     redirect "/songs/#{song.slug}"
+#   end
+# 
+#   get '/songs/:slug' do
+#     @song = Song.find_by_slug(params[:slug])
+#     erb :"songs/show"
+#   end
+# 
+#   get '/songs/:slug/edit' do
+#     @genres = Genre.all
+#     @song = Song.find_by_slug(params[:slug])
+#     erb :'/songs/edit'
+#   end
+# 
+#   patch '/songs/:slug' do
+#     song = Song.find_by_slug(params[:slug])
+#     artist = Artist.find_or_create_by(name: params[:song][:artist_name])
+#     song.update(name: params[:song][:name], artist: artist)
+#     song.genre_ids = params[:song][:genres]
+#     song.save
+# 
+#     flash[:message] = "Sucessfully created song."
+#     redirect "/songs/#{song.slug}"
+#   end
+# 
+# end
 require 'rack-flash'
+require "rack/flash/test"
+
 class SongController < ApplicationController
 
-  # This should present the user with a list of all songs in the library.
-  # Each song should be a clickable link to that particular song's show page.
   get '/songs' do
     @songs = Song.all
     erb :'songs/index'
   end
 
-  # Display the new song form with selectable genres
   get '/songs/new' do
     @genres = Genre.all
+  
     erb :'/songs/new'
   end
 
@@ -20,13 +69,14 @@ class SongController < ApplicationController
     song.genre_ids = params[:song][:genres]
     song.save
 
-    flash[:message] = "Sucessfully created song."
+    flash[:message] = "Successfully created song."
     redirect "/songs/#{song.slug}"
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    erb :"songs/show"
+
+    erb :'songs/show'
   end
 
   get '/songs/:slug/edit' do
@@ -41,8 +91,8 @@ class SongController < ApplicationController
     song.update(name: params[:song][:name], artist: artist)
     song.genre_ids = params[:song][:genres]
     song.save
-
-    flash[:message] = "Sucessfully created song."
+    
+    flash[:message] = "Successfully updated song."
     redirect "/songs/#{song.slug}"
   end
 
